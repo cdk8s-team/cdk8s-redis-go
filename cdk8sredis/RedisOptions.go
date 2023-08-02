ce3aed81643127d3ca8629b4afcd49f189dad15d
cdk8s-automation <cdk8s-team@amazon.com>
2023-08-02 00:34:17 +0000

    chore(release): v0.1.680

--- cdk8sredis/RedisOptions.go
+++ cdk8sredis/RedisOptions.go
@@ -4,9 +4,13 @@ package cdk8sredis
 // Experimental.
 type RedisOptions struct {
 	// Extra labels to associate with resources.
+	// Default: - none.
+	//
 	// Experimental.
 	Labels *map[string]*string `field:"optional" json:"labels" yaml:"labels"`
 	// Number of replicas.
+	// Default: 2.
+	//
 	// Experimental.
 	Replicas *float64 `field:"optional" json:"replicas" yaml:"replicas"`
 }
