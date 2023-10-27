acbcb3d9f1dc27ae11101caa83e6edc2c908ddd1
cdk8s-automation <cdk8s-team@amazon.com>
2023-10-27 06:08:54 +0000

    chore(release): v0.1.750

--- cdk8sredis/jsii/jsii.go
+++ cdk8sredis/jsii/jsii.go
@@ -13,7 +13,7 @@ import (
 	cdk8s      "github.com/cdk8s-team/cdk8s-core-go/cdk8s/v2/jsii"
 )
 
-//go:embed cdk8s-redis-0.1.749.tgz
+//go:embed cdk8s-redis-0.1.750.tgz
 var tarball []byte
 
 // Initialize loads the necessary packages in the @jsii/kernel to support the enclosing module.
@@ -24,5 +24,5 @@ func Initialize() {
 	constructs.Initialize()
 
 	// Load this library into the kernel
-	_jsii_.Load("cdk8s-redis", "0.1.749", tarball)
+	_jsii_.Load("cdk8s-redis", "0.1.750", tarball)
 }
